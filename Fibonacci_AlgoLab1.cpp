50a6e7747489a446ade6febf5095ab2594c316d0
Farnaz-01 <farnazfaria12@gmail.com>
2025-10-26 09:26:24 +0600

    First git hub push.

---
+++ Fibonacci_AlgoLab1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int arr[10];
+    int size_= sizeof(arr[])/sizeof(arr[0]);
+    //int arr[size_];
+
+
+    int sum=0;
+
+    for (int i=0;i<size_;i++){
+
+        sum+=0+i;
+       arr[i]=
+
+
+    }
+
+
+    return 0;
+}
